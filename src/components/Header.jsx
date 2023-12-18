98c18fb27eb46769ac99bba49122de0d2e1a7098
sgrazys <s.grazys@gmail.com>
2023-12-18 18:35:13 +0200

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+	return (
+		<header>
+			<h1 className='logo'>
+				PING<span>.</span>
+			</h1>
+			<p className='description'>
+				We are launching <span>soon!</span>
+			</p>
+			<p className='subscribe-msg'>Subscribe and get notified</p>
+		</header>
+	);
+}
+
+export default Header;
